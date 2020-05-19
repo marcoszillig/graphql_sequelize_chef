92d27be0b7214bf895285479e5d9631e1774f8b3
Marcos Zillig <marcos.zillig@gmail.com>
2020-05-19 20:40:59 -0300

    add service proj

--- services/src/graphql/resolvers/index.ts
+++ services/src/graphql/resolvers/index.ts
@@ -1,9 +1,11 @@
-import Chef from './Chef'
+import Chef from "./Chef";
+import * as Mutation from "./Mutation";
 import * as Query from "./Query";
 
 const resolvers = {
   Chef,
-  Query
-}
+  Mutation,
+  Query,
+};
 
 export default resolvers;
